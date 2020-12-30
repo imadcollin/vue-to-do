f22ce1f3e0e276d16b0bbd7950d7291ede4e3990
imadcollin <imad.collin@gmail.com>
2020-12-30 23:08:55 +0100

    Add some mock data

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,32 @@
 <template>
-  <HelloWorld msg="Hello"/>
+  <Todos m v-bind:todos="todos"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Todos from './components/Todos'
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Todos
+  },
+   data(){
+     return {
+       todos:[
+         {id:1,
+         title: "title 1",
+         status: true
+         },
+           {id:2,
+         title: "title 2",
+         status: true
+         },
+           {id:3,
+         title: "title 3",
+         status: false
+         }
+     ]
+     }
+}
 }
 </script>
 
