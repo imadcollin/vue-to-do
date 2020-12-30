1454671ebbebec1f77fbebfd99485f25e82f29cb
imadcollin <imad.collin@gmail.com>
2020-12-30 23:09:06 +0100

    Add a new view

---
+++ src/components/Todos.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="Todos">
+  <h1>{{msg}}</h1>
+  <div v-bind:key="a.id" v-for="a in todos">
+      
+      <h3>Todos {{a.id}} {{a.title}}</h3>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Todos',
+  props: {
+    msg: String,
+    todos: Array 
+  }
+// props:["todos"]
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  color:#000;
+}
+</style>
